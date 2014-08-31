65ce69b2cf7d008db64b20e275ad34dfe3048211
FolarinOmotoriogun <folarinomotoriogun@gmail.com>
2014-08-31 06:14:00 +0100

    Successfully sandbox instances

--- src/main/java/com/withinet/opaas/Application.java
+++ src/main/java/com/withinet/opaas/Application.java
@@ -136,227 +136,230 @@ public class Application extends WebApplication {
 		mountPage("/team", TeamIndex.class);
 		mountPage("/instances", InstanceIndex.class);
 		mountPage("/account", AccountIndex.class);
-
-		User web = new User();
-		web.setCreated(new Date());
-		web.setFullName("Withinet System Team");
-		web.setPlatformName("OSGi Cloud Platform as a Service");
-		web.setStatus("Active");
-		web.setEmail(SUPER_ADMIN_EMAIL);
-		web.setPassword(SUPER_ADMIN_PASSWORD);
-		web.setLocation("United Kingdom");
-		web.setAdministrator(web);
-		web.setQuota(-1);
-		web.setRole("SUPER ADMINISTRATOR");
-		web.setWorkingDirectory("");
-		web.setIntroduction("I am the main adminitrator for this system");
-		userRepo.save(web);
 		
-		User dummy = new User();
-		dummy.setCreated(new Date());
-		dummy.setFullName("Withinet System Team");
-		dummy.setPlatformName("OSGi Cloud Platform as a Service");
-		dummy.setStatus("Active");
-		dummy.setEmail("a@b.com");
-		dummy.setPassword(SUPER_ADMIN_PASSWORD);
-		dummy.setLocation("United Kingdom");
-		dummy.setAdministrator(web);
-		dummy.setQuota(-1);
-		dummy.setRole("SUPER ADMINISTRATOR");
-		dummy.setWorkingDirectory("");
-		dummy.setIntroduction("I am the main adminitrator for this system");
-		userRepo.save(dummy);
-		
-		Permission p1 = new Permission ();
-		p1.setValue("readProject");
-		p1.setDescription("Project: User can read project information including bundles");
-		permissionRepo.save(p1);
-		
-		Permission p2 = new Permission ();
-		p2.setValue("createProject");
-		p2.setDescription("Project: User can create new projects");
-		permissionRepo.save(p2);
-		
-		Permission p3 = new Permission ();
-		p3.setValue("deleteProject");
-		p3.setDescription("Project: User can delete created projects");
-		permissionRepo.save(p3);
-		
-		Permission p4 = new Permission ();
-		p4.setValue("updateProject");
-		p4.setDescription("Project: Update can update project information");
-		permissionRepo.save(p4);
-		
-		Permission p5 = new Permission ();
-		p5.setValue("projectAdmin");
-		p5.setDescription("PROJECT ADMINISTRATOR: User can perform all actions on projects");
-		permissionRepo.save(p5);
-		
-		Permission p29 = new Permission ();
-		p29.setValue("disableProject");
-		p29.setDescription("Project: User can disable created projects");
-		permissionRepo.save(p29);
-		
-		Permission p6 = new Permission ();
-		p6.setValue("createBundle");
-		p6.setDescription("Bundle: User can create project bundles");
-		permissionRepo.save(p6);
-		
-		Permission p7 = new Permission ();
-		p7.setValue("deleteBundle");
-		p7.setDescription("Bundle: User can delete project bundles");
-		permissionRepo.save(p7);
-		
-		Permission p8 = new Permission ();
-		p8.setValue("readBundle");
-		p8.setDescription("Bundle: User can view bundle information");
-		permissionRepo.save(p8);
-		
-		Permission p9 = new Permission ();
-		p9.setValue("updateBundle");
-		p9.setDescription("Bundle: User can update bundle information");
-		permissionRepo.save(p9);
-		
-		Permission p10 = new Permission ();
-		p10.setValue("bundleAdmin");
-		p10.setDescription("BUNDLE ADMINISTRATOR: User can perform all CRUD actions on bundles");
-		permissionRepo.save(p10);
-		
-		Permission p11 = new Permission ();
-		p11.setValue("createInstance");
-		p11.setDescription("Instance: User can create platform instances of a project");
-		permissionRepo.save(p11);
-		
-		Permission p12 = new Permission ();
-		p12.setValue("readInstance");
-		p12.setDescription("Instance: User can read platform instance information except log or console");
-		permissionRepo.save(p12);
-		
-		Permission p13 = new Permission ();
-		p13.setValue("deleteInstance");
-		p13.setDescription("Instance: User can delete platform instances");
-		permissionRepo.save(p13);
-		
-		Permission p27 = new Permission ();
-		p27.setValue("stopInstance");
-		p27.setDescription("Instance: User can stop live platform instances");
-		permissionRepo.save(p27);
-		
-		Permission p28 = new Permission ();
-		p28.setValue("startInstance");
-		p28.setDescription("Instance: User can start dead platform instances");
-		permissionRepo.save(p28);
-		
-		Permission p30 = new Permission ();
-		p30.setValue("instanceConsole");
-		p30.setDescription("Instance: User can access instance management console");
-		permissionRepo.save(p30);
-		
-		Permission p40 = new Permission ();
-		p40.setValue("instanceLog");
-		p40.setDescription("Instance: User can access instance logs");
-		permissionRepo.save(p40);
-		
-		Permission p14 = new Permission ();
-		p14.setValue("instanceAdmin");
-		p14.setDescription("INSTANCE ADMINISTRATOR: User can perform all operations on instances");
-		permissionRepo.save(p14);
-		
-		Permission p15 = new Permission ();
-		p15.setValue("signedIn");
-		p15.setDescription("Session: User can sign into system");
-		permissionRepo.save(p15);
-		
-		/*Permission p16 = new Permission ();
-		p16.setValue("readUser");
-		p16.setDescription("Read user");
-		permissionRepo.save(p16);
-		
-		Permission p17 = new Permission ();
-		p17.setValue("deleteUser");
-		p17.setDescription("Delete user");
-		permissionRepo.save(p17);
-		
-		Permission p18 = new Permission ();
-		p18.setValue("updateUser");
-		p18.setDescription("Update user");
-		permissionRepo.save(p18);
-		
-		Permission p19 = new Permission ();
-		p19.setValue("userAdmin");
-		p19.setDescription("User Admin");
-		permissionRepo.save(p19);*/
-		
-		/*Permission p20 = new Permission ();
-		p20.setValue("createRole");
-		p20.setDescription("Create Role");
-		permissionRepo.save(p20);
-		
-		Permission p21 = new Permission ();
-		p21.setValue("deleteRole");
-		p21.setDescription("Delete Role");
-		permissionRepo.save(p21);
-		
-		Permission p22 = new Permission ();
-		p22.setValue("readRole");
-		p22.setDescription("Read Role");
-		permissionRepo.save(p22);
-		
-		Permission p23 = new Permission ();
-		p23.setValue("updateRole");
-		p23.setDescription("Update Role");
-		permissionRepo.save(p23);
-		
-		Permission p24 = new Permission ();
-		p24.setValue("adminRole");
-		p24.setDescription("Role Admin");
-		permissionRepo.save(p24);*/
-		/*
-		Permission p25 = new Permission ();
-		p25.setValue("superAdminRole");
-		p25.setDescription("Super Admin");
-		permissionRepo.save(p25);*/
-		
-		Permission p26 = new Permission ();
-		p26.setValue("admin");
-		p26.setDescription("PLATFORM OWNER: Full access to Projects, Instances, Bundles, and Teams");
-		permissionRepo.save(p26);
-		
-		List<Permission> admin = Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, 
-				p26, p27, p28, p29, p30, p40);
-		
-		Role role0 = new Role ();
-		role0.setName(SUPER_ADMIN_NAME);
-		role0.setOwner(web);
-		roleRepo.save(role0);
-		
-		Role role = new Role ();
-		role.setName("ADMINISTRATOR");
-		role.setOwner(web);
-		roleRepo.save(role);
-		
-		Role role2 = new Role ();
-		role2.setName("AUTHENTICATED");
-		role2.setOwner(web);
-		roleRepo.save(role2);
-		
-		web.setAssignedRole(role0);
-		dummy.setAssignedRole(role2);
-		userRepo.save(web);
-		userRepo.save(dummy);
-		
-		//Important to call the role controller to initialize permissions
-		RolePermission rp = new RolePermission (web.getFullName(), role0, p26);
-		rpRepo.save(rp);
-		
-		try {
-			roleCtrl.addPermission(role.getId(), admin, web.getID());
-			roleCtrl.addPermission(role0.getId(), admin, web.getID());
-			roleCtrl.addPermission(role2.getId(), p15, web.getID());
-		} catch (RoleControllerException e) {
-			e.printStackTrace();
-			throw new RuntimeException (e.getMessage());
-		}	
+		if (userRepo.findByRole("SUPER ADMINISTRATOR").size() == 0) {
+			User web = new User();
+			web.setCreated(new Date());
+			web.setFullName("Withinet System Team");
+			web.setPlatformName("OSGi Cloud Platform as a Service");
+			web.setStatus("Active");
+			web.setEmail(SUPER_ADMIN_EMAIL);
+			web.setPassword(SUPER_ADMIN_PASSWORD);
+			web.setLocation("United Kingdom");
+			web.setAdministrator(web);
+			web.setQuota(-1);
+			web.setRole("SUPER ADMINISTRATOR");
+			web.setWorkingDirectory("");
+			web.setIntroduction("I am the main adminitrator for this system");
+			userRepo.save(web);
+			
+			User dummy = new User();
+			dummy.setCreated(new Date());
+			dummy.setFullName("Withinet System Team");
+			dummy.setPlatformName("OSGi Cloud Platform as a Service");
+			dummy.setStatus("Active");
+			dummy.setEmail("a@b.com");
+			dummy.setPassword(SUPER_ADMIN_PASSWORD);
+			dummy.setLocation("United Kingdom");
+			dummy.setAdministrator(web);
+			dummy.setQuota(-1);
+			dummy.setRole("SUPER ADMINISTRATOR");
+			dummy.setWorkingDirectory("");
+			dummy.setIntroduction("I am the main adminitrator for this system");
+			userRepo.save(dummy);
+			
+			Permission p1 = new Permission ();
+			p1.setValue("readProject");
+			p1.setDescription("Project: User can read project information including bundles");
+			permissionRepo.save(p1);
+			
+			Permission p2 = new Permission ();
+			p2.setValue("createProject");
+			p2.setDescription("Project: User can create new projects");
+			permissionRepo.save(p2);
+			
+			Permission p3 = new Permission ();
+			p3.setValue("deleteProject");
+			p3.setDescription("Project: User can delete created projects");
+			permissionRepo.save(p3);
+			
+			Permission p4 = new Permission ();
+			p4.setValue("updateProject");
+			p4.setDescription("Project: Update can update project information");
+			permissionRepo.save(p4);
+			
+			Permission p5 = new Permission ();
+			p5.setValue("projectAdmin");
+			p5.setDescription("PROJECT ADMINISTRATOR: User can perform all actions on projects");
+			permissionRepo.save(p5);
+			
+			Permission p29 = new Permission ();
+			p29.setValue("disableProject");
+			p29.setDescription("Project: User can disable created projects");
+			permissionRepo.save(p29);
+			
+			Permission p6 = new Permission ();
+			p6.setValue("createBundle");
+			p6.setDescription("Bundle: User can create project bundles");
+			permissionRepo.save(p6);
+			
+			Permission p7 = new Permission ();
+			p7.setValue("deleteBundle");
+			p7.setDescription("Bundle: User can delete project bundles");
+			permissionRepo.save(p7);
+			
+			Permission p8 = new Permission ();
+			p8.setValue("readBundle");
+			p8.setDescription("Bundle: User can view bundle information");
+			permissionRepo.save(p8);
+			
+			Permission p9 = new Permission ();
+			p9.setValue("updateBundle");
+			p9.setDescription("Bundle: User can update bundle information");
+			permissionRepo.save(p9);
+			
+			Permission p10 = new Permission ();
+			p10.setValue("bundleAdmin");
+			p10.setDescription("BUNDLE ADMINISTRATOR: User can perform all CRUD actions on bundles");
+			permissionRepo.save(p10);
+			
+			Permission p11 = new Permission ();
+			p11.setValue("createInstance");
+			p11.setDescription("Instance: User can create platform instances of a project");
+			permissionRepo.save(p11);
+			
+			Permission p12 = new Permission ();
+			p12.setValue("readInstance");
+			p12.setDescription("Instance: User can read platform instance information except log or console");
+			permissionRepo.save(p12);
+			
+			Permission p13 = new Permission ();
+			p13.setValue("deleteInstance");
+			p13.setDescription("Instance: User can delete platform instances");
+			permissionRepo.save(p13);
+			
+			Permission p27 = new Permission ();
+			p27.setValue("stopInstance");
+			p27.setDescription("Instance: User can stop live platform instances");
+			permissionRepo.save(p27);
+			
+			Permission p28 = new Permission ();
+			p28.setValue("startInstance");
+			p28.setDescription("Instance: User can start dead platform instances");
+			permissionRepo.save(p28);
+			
+			Permission p30 = new Permission ();
+			p30.setValue("instanceConsole");
+			p30.setDescription("Instance: User can access instance management console");
+			permissionRepo.save(p30);
+			
+			Permission p40 = new Permission ();
+			p40.setValue("instanceLog");
+			p40.setDescription("Instance: User can access instance logs");
+			permissionRepo.save(p40);
+			
+			Permission p14 = new Permission ();
+			p14.setValue("instanceAdmin");
+			p14.setDescription("INSTANCE ADMINISTRATOR: User can perform all operations on instances");
+			permissionRepo.save(p14);
+			
+			Permission p15 = new Permission ();
+			p15.setValue("signedIn");
+			p15.setDescription("Session: User can sign into system");
+			permissionRepo.save(p15);
+			
+			/*Permission p16 = new Permission ();
+			p16.setValue("readUser");
+			p16.setDescription("Read user");
+			permissionRepo.save(p16);
+			
+			Permission p17 = new Permission ();
+			p17.setValue("deleteUser");
+			p17.setDescription("Delete user");
+			permissionRepo.save(p17);
+			
+			Permission p18 = new Permission ();
+			p18.setValue("updateUser");
+			p18.setDescription("Update user");
+			permissionRepo.save(p18);
+			
+			Permission p19 = new Permission ();
+			p19.setValue("userAdmin");
+			p19.setDescription("User Admin");
+			permissionRepo.save(p19);*/
+			
+			/*Permission p20 = new Permission ();
+			p20.setValue("createRole");
+			p20.setDescription("Create Role");
+			permissionRepo.save(p20);
+			
+			Permission p21 = new Permission ();
+			p21.setValue("deleteRole");
+			p21.setDescription("Delete Role");
+			permissionRepo.save(p21);
+			
+			Permission p22 = new Permission ();
+			p22.setValue("readRole");
+			p22.setDescription("Read Role");
+			permissionRepo.save(p22);
+			
+			Permission p23 = new Permission ();
+			p23.setValue("updateRole");
+			p23.setDescription("Update Role");
+			permissionRepo.save(p23);
+			
+			Permission p24 = new Permission ();
+			p24.setValue("adminRole");
+			p24.setDescription("Role Admin");
+			permissionRepo.save(p24);*/
+			/*
+			Permission p25 = new Permission ();
+			p25.setValue("superAdminRole");
+			p25.setDescription("Super Admin");
+			permissionRepo.save(p25);*/
+			
+			Permission p26 = new Permission ();
+			p26.setValue("admin");
+			p26.setDescription("PLATFORM OWNER: Full access to Projects, Instances, Bundles, and Teams");
+			permissionRepo.save(p26);
+			
+			List<Permission> admin = Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, 
+					p26, p27, p28, p29, p30, p40);
+			
+			Role role0 = new Role ();
+			role0.setName(SUPER_ADMIN_NAME);
+			role0.setOwner(web);
+			roleRepo.save(role0);
+			
+			Role role = new Role ();
+			role.setName("ADMINISTRATOR");
+			role.setOwner(web);
+			roleRepo.save(role);
+			
+			Role role2 = new Role ();
+			role2.setName("AUTHENTICATED");
+			role2.setOwner(web);
+			roleRepo.save(role2);
+			
+			web.setAssignedRole(role0);
+			dummy.setAssignedRole(role2);
+			userRepo.save(web);
+			userRepo.save(dummy);
+			
+			//Important to call the role controller to initialize permissions
+			RolePermission rp = new RolePermission (web.getFullName(), role0, p26);
+			rpRepo.save(rp);
+			
+			try {
+				roleCtrl.addPermission(role.getId(), admin, web.getID());
+				roleCtrl.addPermission(role0.getId(), admin, web.getID());
+				roleCtrl.addPermission(role2.getId(), p15, web.getID());
+			} catch (RoleControllerException e) {
+				e.printStackTrace();
+				throw new RuntimeException (e.getMessage());
+			}	
+		}
+	
 	}
 
 	@Autowired
